18751f8c83056147f18880aa4edce9f3127ede69
CyranHOEdeBergerac <danael.carbonneau@protonmail.com>
2023-03-27 13:13:37 +0200

    Monade State

--- pcompProject/src/StateConfig.hs
+++ pcompProject/src/StateConfig.hs
@@ -6,3 +6,33 @@ data GameConfig = GameConfig{
     device :: Int
 }
 
+changeDevice :: Int -> State GameConfig Int
+changeDevice newDevice = do
+    options <- get
+    put $ options {device = newDevice}
+    return (newDevice)
+
+changeInstrument :: Int -> State GameConfig Int
+changeDevice newInstrument = do 
+    options <- get
+    put $ options {instrument = newInstrument}
+    return (newInstrument)
+
+changeMode :: Int -> State GameConfig Int
+changeMode newMode = do
+    options <- get
+    put $ options {mode = newMode}
+    return (newMode)
+
+changeMirror :: Bool -> State GameConfig Bool
+changeMirror newMirroir = do
+    options <- get
+    put $ options {mirroir = newMirroir}
+    return (newMirroir)
+
+changeF :: Float -> State GameConfig Float
+changeF newF = do
+    options <- get
+    put $ options {f = newF}
+    return (newF)
+
