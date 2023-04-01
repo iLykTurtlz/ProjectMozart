042795a314f510f17664899ef43d3cf1ba8f50d8
Paul Jarski <pjarski@sfcm.edu>
2023-04-01 14:15:08 +0200

    en progres IOMenu

--- pcompProject/test/pauls_tests.hs
+++ pcompProject/test/pauls_tests.hs
@@ -2,3 +2,12 @@
 
 test::IO ()
 test = putStrLn "hello, world"
+
+
+concatInput::IO ()
+concatInput = do
+  putStrLn "Enter a number"
+  input <- getLine
+  case readMaybe input of
+    Just a -> putStrLn ("You entered "++(show a))
+    Nothing -> putStrLn "Invalid number entered" 
