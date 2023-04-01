794548779d94b100bed2f2384e36b37b221c1953
CyranHOEdeBergerac <danael.carbonneau@protonmail.com>
2023-04-01 13:11:13 +0200

    IOMenu

--- pcompProject/src/IOMenu.hs
+++ pcompProject/src/IOMenu.hs
@@ -12,36 +12,39 @@ import StateConfig
 --On a besoin d'une configuration pour lancer un menu, au premier appel, ce sera la configuration de base.
 menu :: GameConfig -> IO()          
 menu config = do
-    putStrLn "Le Jeu de Mozart\nQue voulez vous faire ?\n"
-    putStrLn " 0 : Quitter\n 1 : Jouer un Menuet\n 2 : Changer la configuration\n"
-    c <- getChar
-    case c of
-        "0" -> putStrLn "Au revoir"
-        "1" -> putStrLn "On joue"
-        "2" -> putStrLn "Autre Menu"
-        _   -> do
-		putStrLn "Mauvaise commande : "++c++"; on relance."
-		menu config
-    terminate
-    return()
+  putStrLn "Le Jeu de Mozart\nQue voulez vous faire ?\n"
+  putStrLn " 0 : Quitter\n 1 : Jouer un Menuet\n 2 : Changer la configuration\n"
+  c <- getChar
+  case c of
+    '0' -> putStrLn "Au revoir"
+    '1' -> putStrLn "On joue"
+    '2' -> putStrLn "Autre Menu"
+    _   -> do
+      putStrLn "Mauvaise commande"
+  terminate
+  return()
 
 --Fonction menuConfig, avec un paramètre de type GameConfig, 
 --fonction qui affiche les choix et fait les changements de configuration correspondant, et retourne la nouvelle configuration
 menuConfig :: GameConfig -> IO GameConfig
 menuConfig config = do
-    putStrLn "\tQuel changement de configuration apporter ?\n"
-    putStrLn "\t 0 : Aucun\n\t 1 : Changer d'instrument\n\t 2 : Mode de transposition\n\t 3 : Mode mirroir ?\n\t 4 : Multiplier la durée du menuet\n\t 5 : Changer l'appareil de Sortie"
-    --Traiter les différents cas en faisant appel aux fonctions de StateConfig
-    --Si 1,2,3,4,5 en entrée, on fait un appel récursif à GameConfig avec la nouvelle 
-    --configuration récupérée, sinon, on la retourne juste.
-    c <- getChar
-    case c of 
-        "1" ->
-        "2" ->
-        "3" ->
-        "4" ->
-        "5" ->
-        _ ->        -- Cas d'arrêt
+  putStrLn "\tQuel changement de configuration apporter ?\n"
+  putStrLn "\t 0 : Aucun\n\t 1 : Changer d'instrument\n\t 2 : Mode de transposition\n\t 3 : Mode mirroir ?\n\t 4 : Multiplier la durée du menuet\n\t 5 : Changer l'appareil de Sortie"
+  --Traiter les différents cas en faisant appel aux fonctions de StateConfig
+  --Si 1,2,3,4,5 en entrée, on fait un appel récursif à GameConfig avec la nouvelle 
+  --configuration récupérée, sinon, on la retourne juste.
+  c <- getChar
+  case c of 
+    '0' -> putStrLn "Au revoir"
+    '1' -> putStrLn "Instrument"
+    '2' -> putStrLn "Transposition"
+    '3' -> putStrLn "Miroir"
+    '4' -> putStrLn "Durée"
+    '5' -> putStrLn "Device"
+    _ -> do
+      putStrLn "Mauvaise Commande"
+  terminate
+  return config
     
 
 
@@ -49,7 +52,7 @@ menuConfig config = do
 
 
 --Fonction jouerMenuet, avec la configuration en paramètres
-jouerMenuet :: GameConfig -> IO()
+--jouerMenuet :: GameConfig -> IO()
 
 --Il faut voir où mettre l'ouverture de stream, si on la met avant de jouer le menuet, on peut alors passer simplement
 --Le stream en paramètres, on verra globalement ça avec la 4.4
