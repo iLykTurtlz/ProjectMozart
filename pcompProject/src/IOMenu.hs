b3aad8693167f593dda7617b9cd16fc5231e7102
CyranHOEdeBergerac <danael.carbonneau@protonmail.com>
2023-04-01 15:35:54 +0200

    IOMenu

--- pcompProject/src/IOMenu.hs
+++ pcompProject/src/IOMenu.hs
@@ -22,8 +22,10 @@ menu config = do
         putStrLn "\nOn joue\n"
         menu config
     '2' -> do
-        putStrLn "\nAutre Menu\n"
-        menu config
+        putStrLn "\nOn va vers l'autre Menu\n"
+        newConfig <- menuConfig config
+        putStrLn (show newConfig)
+        menu newConfig
     _   -> do
       putStrLn "\nMauvaise commande\n"
       menu config
@@ -33,7 +35,7 @@ menu config = do
 --Fonction menuConfig, avec un paramètre de type GameConfig, 
 --fonction qui affiche les choix et fait les changements de configuration correspondant, et retourne la nouvelle configuration
 menuConfig :: GameConfig -> IO GameConfig
-menuConfig config = do
+menuConfig mconfig = do
   putStrLn "\tQuel changement de configuration apporter ?\n"
   putStrLn "\t 0 : Aucun\n\t 1 : Changer d'instrument\n\t 2 : Mode de transposition\n\t 3 : Mode mirroir ?\n\t 4 : Multiplier la durée du menuet\n\t 5 : Changer l'appareil de Sortie"
   --Traiter les différents cas en faisant appel aux fonctions de StateConfig
@@ -41,61 +43,13 @@ menuConfig config = do
   --configuration récupérée, sinon, on la retourne juste.
   c <- getChar
   case c of 
-    '0' -> do 
-        putStrLn "Au revoir"
-        return config
-    '1' -> do
-        putStrLn "Changement d'instrument"
-        putStrLn "\n\t\tEntrez un nombre entre 1 et 5\n"
-        i <- getInt
-        (_,newConfig) <- runState (changeInstrument i) config
-        menuConfig newConfig
-    '2' -> do
-        putStrLn "\n\t\tTransposition"
-        putStrLn "\n\t\tEntrer un mode de transposition \n(0 : pas de transposition, 1: transposition de +12 demis tons, 2 : transpositon de -12 demis tons, 3 : transposition libre)\n"
-        t <- getChar
-        case t of
-            '0' -> "\n\t\t~Au revoir !"
-            '1' -> do
-                (_,newConfig)<- runState (changeMode 1) config
-                menuConfig newConfig
-            '2' -> do
-                (_,newConfig)<- runState (changeMode 2) config
-                menuConfig newConfig
-            '3' -> do
-                putStrLn "En travaux"
-                menuConfig config
-            _ -> do 
-                putStrLn "\n\t\tMauvaise Commande\n"
-                menuConfig config
-
-    '3' -> do
-        putStrLn "\n\t\tMode Miroir"
-        putStrLn "\n\t\ty/n ?"
-        m <- getChar
-        case m of 
-            'y'->do
-                (_,newConfig)<- runState (changeMirror True) config
-                menuConfig newConfig
-            'n'->do
-                (_,newConfig)<- runState (changeMirror False) config
-                menuConfig newConfig
-    '4' -> do
-        putStrLn "\n\t\tMultiplier la durée"
-        putStrLn "\n\t\tEntrez un nombre flottant pour multiplier la durée du menuet\n"
-        d <- getFloat
-        (_,newConfig)<- runState (changeF d) config
-        menuConfig newConfig
-    '5' -> do
-        putStrLn "\n\t\tChanger l'appareil de sortie"
-        putStrLn "\n\t\tEntrez le numéro de l'appareil de sortie souhaité\n"
-        device <- getInt
-        (_,newConfig)<- runState (changeDevice device) config
-        menuConfig newConfig
-    _ -> do
-      putStrLn "Mauvaise Commande"
+    '0' -> putStrLn "\n\t~ À Plus !\n\n"    --CAS 0 OK
+    
+    _ -> do 
+        putStrLn "LOL\n"
+        menuConfig mconfig
   terminate
-  return config
+  return mconfig
     
 
 
