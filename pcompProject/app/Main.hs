033d466243daf97ed678adc59146f6bd819211b7
CyranHOEdeBergerac <danael.carbonneau@protonmail.com>
2023-04-02 20:16:56 +0200

    Main

--- pcompProject/app/Main.hs
+++ pcompProject/app/Main.hs
@@ -15,7 +15,7 @@ main = do
   initialize
   deviceId <- getDefaultOutputDeviceID
   case deviceId of
-    Just id ->  menu (GameConfig 1 0 False 1.0 id 0)      --On entre dans le menu du jeu avec un id par défaut
-    Nothing -> menu (GameConfig 1 0 False 1.0 0 0)         --On entre dans le menu sans id par défaut
+    Just id ->  menu (GameConfig 1 0 False 1.0 id 0 60)      --On entre dans le menu du jeu avec un id par défaut
+    Nothing -> menu (GameConfig 1 0 False 1.0 0 0 60)         --On entre dans le menu sans id par défaut
   return ()
 
